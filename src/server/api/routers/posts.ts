7ff8c6a8e49aed4172c3c86967acac6d2f90c419
apm30 <m.ajaypradeep@icloud.com>
2024-03-09 14:30:58 +0000

    post mutilation in the backend with author

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,28 +1,60 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/backend/dist/types/api/resources/User";
+
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+/*  Note :
+ * import type { User } from "@clerk/nextjs/dist/api - No longer exists
+ *  import type { User } from "@clerk/backend/dist/types/api/resources/User" - New import
+ */
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    userName: user.username,
+    profilePicture: user.imageUrl,
+  };
+};
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+        limit: 100,
+      })
+    ).map(filterUserForClient);
+
+    console.log("Users", users);
+
+    return posts.map((post) => ({
+      ...post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
-
-  // create: publicProcedure
-  //   .input(z.object({ name: z.string().min(1) }))
-  //   .mutation(async ({ ctx, input }) => {
-  //     // simulate a slow db call
-  //     await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  //     return ctx.db.post.create({
-  //       data: {
-  //         name: input.name,
-  //       },
-  //     });
-  //   }),
-
-  // getLatest: publicProcedure.query(({ ctx }) => {
-  //   return ctx.db.post.findFirst({
-  //     orderBy: { createdAt: "desc" },
-  //   });
-  // }),
 });
+
+// create: publicProcedure
+//   .input(z.object({ name: z.string().min(1) }))
+//   .mutation(async ({ ctx, input }) => {
+//     // simulate a slow db call
+//     await new Promise((resolve) => setTimeout(resolve, 1000));
+
+//     return ctx.db.post.create({
+//       data: {
+//         name: input.name,
+//       },
+//     });
+//   }),
+
+// getLatest: publicProcedure.query(({ ctx }) => {
+//   return ctx.db.post.findFirst({
+//     orderBy: { createdAt: "desc" },
+//   });
+// }),
