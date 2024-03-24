c36908b49b62e00f1a49479a11d7cd0561c4831d
apm30 <m.ajaypradeep@icloud.com>
2024-03-24 20:38:48 +0000

    first mutation create post

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,11 @@ import type { User } from "@clerk/backend/dist/types/api/resources/User";
 
 import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 /*  Note :
  * import type { User } from "@clerk/nextjs/dist/api - No longer exists
@@ -60,3 +64,22 @@ export const postRouter = createTRPCRouter({
 //     orderBy: { createdAt: "desc" },
 //   });
 // }),
+
+create: privateProcedure
+  .input(
+    z.object({
+      content: z.string().emoji().min(1).max(280),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    const authorId = ctx.currentUser.id;
+
+    const post = await ctx.db.post.create({
+      data: {
+        authorId,
+        content: input.content,
+      },
+    });
+
+    return post;
+  });
--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -6,9 +6,10 @@
  * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
  * need to use are documented accordingly near the end.
  */
-import { getAuth } from "@clerk/nextjs/server";
-import { initTRPC } from "@trpc/server";
+import { currentUser, getAuth } from "@clerk/nextjs/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
+import { error } from "console";
 import superjson from "superjson";
 import { ZodError } from "zod";
 
@@ -31,11 +32,13 @@ import { db } from "~/server/db";
 export const createTRPCContext = (opts: CreateNextContextOptions) => {
   const { req } = opts;
 
-  const user = getAuth(req);
+  const session = getAuth(req);
+
+  const user = session?.user;
 
   return {
     db,
-    session: user,
+    currentUser: user,
   };
 };
 
@@ -83,3 +86,24 @@ export const createTRPCRouter = t.router;
  * are logged in.
  */
 export const publicProcedure = t.procedure;
+
+/**
+ * Authenticated procedure
+ *
+ * This is the same as `publicProcedure`, but it requires that the user is logged in to access it.
+ */
+const enforceUserIsAuth = t.middleware(async ({ ctx, next }) => {
+  if (!ctx.currentUser) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to perform this action",
+    });
+  }
+  return next({
+    ctx: {
+      currentUser: ctx.currentUser,
+    },
+  });
+});
+
+export const privateProcedure = t.procedure.use(enforceUserIsAuth);
